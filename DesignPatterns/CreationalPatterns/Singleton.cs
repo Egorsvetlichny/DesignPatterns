9dadf57e01fee3eacac15a70be77c26b7f591e85
Egor Svetlichny <fkhor@inbox.ru>
2024-09-15 16:55:48 +0300

    Add Singleton pattern realization

---
+++ DesignPatterns/CreationalPatterns/Singleton.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.CreationalPatterns
+{
+    public class Singleton
+    {
+        private static Singleton instance = null;
+        private static readonly object padlock = new object();
+
+        private Singleton() { }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Singleton();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        public void ConnecrToDb()
+        {
+            Console.WriteLine($"Подключение к базе данных из объекта {instance}");
+        }
+    }
+}
--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using DesignPatterns.CreationalPatterns;
 
 namespace DesignPatterns
 {
@@ -6,7 +7,17 @@ namespace DesignPatterns
     {
         public static void Main(string[] args)
         {
+            SingltonWork();
+        }
 
+        public static void SingltonWork()
+        {
+            //Ошибка из-за приватного конструктора
+            //Singleton singleton = new Singleton();
+            
+            Singleton singleton = Singleton.Instance;
+            Singleton newSingleton = Singleton.Instance;
+            Console.WriteLine(singleton == newSingleton);
         }
 
     }
